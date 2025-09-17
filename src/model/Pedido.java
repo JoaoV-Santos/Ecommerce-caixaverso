5613172d2d59af4c7bc121f26f426a6c3a4a152a
João Victor <joaovictorsantos10@hotmail.com.br>
2025-09-17 20:43:46 -0300

    atualizado classe Pedido

--- src/model/Pedido.java
+++ src/model/Pedido.java
@@ -1,10 +1,27 @@
 package model;
 
+import model.enums.StatusPagamento;
+import model.enums.StatusPedido;
+
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class Pedido {
     private int id;
+    private static int contador = 1;
     private Cliente cliente;
     LocalDateTime dataHora;
-    
+    StatusPedido statusPedido;
+    StatusPagamento statusPagamento;
+    private List<ItemPedido> itens;
+
+    public Pedido (Cliente cliente ) {
+        this.id = contador;
+        contador++;
+        dataHora = LocalDateTime.now();
+        this.cliente = cliente;
+        this.statusPedido = StatusPedido.ABERTO;
+        this.statusPagamento = StatusPagamento.NENHUM;
+
+    }
 }
