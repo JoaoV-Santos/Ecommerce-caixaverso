464e352507ea4a9c14d1d77cea634777b9086600
João Victor <joaovictorsantos10@hotmail.com.br>
2025-09-17 20:21:37 -0300

    classes Pedido e Produto corrigidas

--- src/model/Pedido.java
+++ src/model/Pedido.java
@@ -1,22 +1,5 @@
 package model;
 
 public class Pedido {
-    private String nome;
-    private double valorCadastrado;
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public double getValorCadastrado() {
-        return valorCadastrado;
-    }
-
-    public void setValorCadastrado(double valorCadastrado) {
-        this.valorCadastrado = valorCadastrado;
-    }
 }
--- src/model/Produto.java
+++ src/model/Produto.java
@@ -1,4 +1,22 @@
 package model;
 
 public class Produto {
+    private String nome;
+    private double precoCadastrado;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPrecoCadastrado() {
+        return precoCadastrado;
+    }
+
+    public void setPrecoCadastrado(double precoCadastrado) {
+        this.precoCadastrado = precoCadastrado;
+    }
 }
