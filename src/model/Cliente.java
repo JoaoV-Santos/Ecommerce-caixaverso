f4a2217d4141aa3040f94cb86054cef2cb7b1705
João Victor <joaovictorsantos10@hotmail.com.br>
2025-09-17 20:12:13 -0300

    criada classe Cliente

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,4 +1,40 @@
 package model;
 
 public class Cliente {
+    private String nome;
+    private String cpf;
+    private String email;
+
+    public Cliente(String nome, String cpf, String email) {
+        this.nome = nome;
+        this.email = email;
+        setCpf(cpf);
+    }
+
+    public void setCpf(String cpf) {
+        if (cpf == null || cpf.isEmpty()) {
+            throw new IllegalArgumentException("CPF obrigatório");
+        }
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
